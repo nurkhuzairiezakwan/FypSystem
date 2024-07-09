36dbd336af6b75cfc649b731b5276e3b2961f2bd
Wan Amirul <wanamirulhafiq@graduate.utm.my>
2024-07-09 21:56:19 +0800

    push models 1

---
+++ ADStarter.Models/ProjectType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADStarter.Models
+{
+    public class ProjectType
+    {
+        public int pt_id { get; set; }
+        public string pt_desc { get; set; }
+
+      
+    }
+}
